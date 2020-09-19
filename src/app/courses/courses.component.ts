2574f8c3f73fc78fb2b1543e95418ba057694041
Chutipong Roobklom <khunemz@gmail.com>
2020-09-20 03:26:29 +0700

    do assignment

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
+  currentCourse: any = null;
   courses = [
     {
       id: 1,
@@ -13,12 +14,36 @@ export class CoursesComponent implements OnInit {
       description: 'Learn the fundamentals of Angular 9',
       percentComplete: 26,
       favorite: true
+    } , 
+    {
+      id: 2,
+      title: 'React Fundamentals',
+      description: 'Learn the fundamentals of Reacdt',
+      percentComplete: 30,
+      favorite: true
+    } , 
+    {
+      id: 3,
+      title: 'HTML course',
+      description: 'HTML Javascript',
+      percentComplete: 30,
+      favorite: true
     }
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectCourse(course) : void {
+    this.currentCourse = course;
+  }
+
+  deleteCourse(id): void {
+    console.log('delete course : ' , id);
+
+    
   }
 
 }
